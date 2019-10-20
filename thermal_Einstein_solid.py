c2672ff0cb8a04e0936dc65d47b5c13ce2f533f2
Ruoqian Zhang <31377103+voidii@users.noreply.github.com>
2019-10-20 14:22:11 -0700

    added input and counter for number of permutation

--- thermal_Einstein_solid.py
+++ thermal_Einstein_solid.py
@@ -8,15 +8,19 @@ N = int(input("number of N:"))
 q = int(input("number of q:"))
 
 list = []
-for num in range(0, 100):
+for num in range(0, 10):
 	list.append(num)
 ls = itertools.product(list, repeat = N)
  
 #遍历所有组合情况
+counter = 0
 for l in ls:
-    if(math.fsum(l) != q):
-        continue
-    else:
-        print (l)
+	if(math.fsum(l) != q):
+		continue
+	else:
+		counter = counter + 1
+		print (l)
+
+print (counter)
 	
 time.sleep(1500);
